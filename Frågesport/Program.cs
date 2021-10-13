dd009637f98f41c812bb2d7b88bc14352c0e3c46
Linnea Winqvist <winqvist.linnea@elev.ga.ntig.se>
2021-10-13 13:30:09 +0200

    drtyhytdhdfydfhn

--- Frågesport/Program.cs
+++ Frågesport/Program.cs
@@ -3,6 +3,38 @@
 int poäng = 0;
 
 Console.WriteLine("Vad ska en kanin äta mest utav? a) morötter, b) hö, c) bark");
+string mat = Console.ReadLine();
 
+if (mat == "b"){
+    poäng = poäng + 1;
+}
 
+Console.WriteLine("Vad är det bästa underlaget för en kanin? a) trägolv, b) stenplattor, c) mjukta mattor");
+string underlag = Console.ReadLine();
+
+if (underlag == "c"){
+    poäng = poäng + 1;
+}
+
+Console.WriteLine("När byter kaniner päls? a) aldrig, b) på hösten, c) på hösten och våren");
+string päls = Console.ReadLine();
+
+if (päls == "c"){
+    poäng = poäng + 1;
+}
+
+Console.WriteLine("Vad betyder det om en kanin stampar i marken? a) den är hungrig, b) den är rädd, c) den varnar");
+string stamp = Console.ReadLine();
+
+if (stamp == "b och c"){
+    poäng = poäng + 2;
+}
+
+if (poäng == 5){
+    Console.WriteLine($"Du fick {poäng}. Grattis!");
+}
+
+if (poäng == 4){
+    Console.WriteLine($"Du fick ")
+}
 Console.ReadLine();
